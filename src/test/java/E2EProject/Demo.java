12a67ab2fb0a7168fc26fed1e0bf8bd8c89a618d
mohanasian2912 <mohan291290@gmail.com>
2021-02-21 14:53:46 +0530

    Mohan commit

--- src/test/java/E2EProject/Demo.java
+++ src/test/java/E2EProject/Demo.java
@@ -26,6 +26,23 @@ public class Demo {
 			System.out.println("\t");
 		}
 		
+		for(int i=0; i<4; i++)
+		{
+			for(int j=0; j<=i; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println("\t");
+		}
+		for(int i=0; i<4; i++)
+		{
+			for(int j=0; j<=i; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println("\t");
+		}
+		
 		
 
 	}
